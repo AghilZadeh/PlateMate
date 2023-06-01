d9c690bf11994fc478de121c7be929e56bf0533a
AghilZadeh <agheal@gmail.com>
2023-06-01 16:50:27 -0400

    added streamlit

---
+++ server/pages/1-FAQ.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="FAQ"
+)
+
+st.write("# FAQ 🧑🏽‍🍳")
+
+
+st.markdown(
+    """
+    Tell me about what you like, I will recommend you recipes to try!
+"""
+)
---
+++ server/pages/2-API.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="API"
+)
+
+st.write("# API 🧑🏽‍🍳")
+
+
+st.markdown(
+    """
+    Tell me about what you like, I will recommend you recipes to try!
+"""
+)
