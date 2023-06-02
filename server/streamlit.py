e49755b9383f83bfd4d8c406de5cd2afe0a90f4b
AghilZadeh <agheal@gmail.com>
2023-06-02 13:02:01 -0400

    svd model added

--- server/streamlit.py
+++ server/streamlit.py
@@ -31,23 +31,7 @@ imgs = [
  'https://img.sndimg.com/food/image/upload/v1/img/feed/54257/xELVM3QvQuCK5oNcuAJ9_IMG_20170212_212029.jpg',
  'https://img.sndimg.com/food/image/upload/v1/img/feed/22782/RSS2jqMfQeu2FGvMkuyg_20170806_182816-01.jpeg',
  'https://img.sndimg.com/food/image/upload/v1/img/feed/32204/PZG96oOQDCIJ4DyeH5Mc_91FD65B7-FB99-4CC2-83BC-A8FBBF991977.jpeg']
-# imgs = [
-#     'https://unsplash.com/photos/-IMlv9Jlb24/download?force=true',
-#     'https://unsplash.com/photos/ESEnXckWlLY/download?force=true',
-#     'https://unsplash.com/photos/mOcdke2ZQoE/download?force=true',
-#     'https://unsplash.com/photos/GPPAjJicemU/download?force=true',
-#     'https://unsplash.com/photos/JFeOy62yjXk/download?force=true',
-#     'https://unsplash.com/photos/kEgJVDkQkbU/download?force=true',
-#     'https://unsplash.com/photos/i9Q9bc-WgfE/download?force=true',
-#     'https://unsplash.com/photos/tIL1v1jSoaY/download?force=true',
-#     'https://unsplash.com/photos/-G3rw6Y02D0/download?force=true',
-#     'https://unsplash.com/photos/xP_AGmeEa6s/download?force=true',
-#     'https://unsplash.com/photos/4iTVoGYY7bM/download?force=true',
-#     'https://unsplash.com/photos/mBQIfKlvowM/download?force=true',
-#     'https://unsplash.com/photos/A-11N8ItHZo/download?force=true',
-#     'https://unsplash.com/photos/kOqBCFsGTs8/download?force=true',
-#     'https://unsplash.com/photos/8DMuvdp-vso/download?force=true'
-# ]
+
 image_iterator = paginator("What do you like?", imgs)
 indices_on_page, images_on_page = map(list, zip(*image_iterator))
 st.image(images_on_page, width=100, caption=indices_on_page)
