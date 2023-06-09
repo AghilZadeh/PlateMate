587d5b4a9f814e3597d037b1644884c042e7fa5e
AghilZadeh <agheal@gmail.com>
2023-06-09 09:08:41 -0400

    experimentation with SVDpp

--- models/MLFlow-SVDpp.py
+++ models/MLFlow-SVDpp.py
@@ -18,8 +18,9 @@ experiment = mlflow.get_experiment_by_name(experiment_name)
 model = SVDpp
 model_name = 'SVDpp'
 model_params = {'n_factors':[5, 10, 20, 50, 100], 'n_epochs':[5, 10, 20, 50, 100]}
+#model_params = {'n_factors':[5], 'n_epochs':[5]}
 #data_params = {'n_data':[1000, 5000, 10000, 50000, 100000]}
-data_params = {'n_data':[1000]}
+data_params = {'n_data':[100000]}
 
 # reading data
 reviews_train = pd.read_csv('./data/reviews_train.csv', usecols=['RecipeId', 'AuthorId', 'Rating'])
